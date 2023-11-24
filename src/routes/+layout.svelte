44d0c47f2bc7eabb3b138feefe856ecea0085250
hummingbard <mail@hummingbard.com>
2023-11-25 02:35:45 +0600

    header import fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Footer from '$lib/components/Footer.svelte';
-	import Header from '$lib/components/Header.svelte';
+	import Header from '$lib/components/header.svelte';
 	import '../app.css';
 </script>
 
